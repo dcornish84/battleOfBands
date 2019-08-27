86677c7a40f06e84eeac82e8b9371f6b185e38ed
David Cornish <52015556+dcornish84@users.noreply.github.com>
2019-08-27 13:48:33 -0500

    Battle of the Bands Practice

---
+++ battle.js
@@ -0,0 +1,10 @@
+let bandNumber = 1
+
+const takeNumber = function (bandName) {
+    console.log(`${bandName} is band number ${bandNumber}`);
+    bandNumber ++;
+    
+}
+
+const scum = takeNumber("Galactic Scum")
+const under = takeNumber("Underdogs")
